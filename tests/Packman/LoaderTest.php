324693a6213e24e156e8be21be22f129a15dd976
Padraic Brady <padraic.brady@yahoo.com>
2011-04-13 18:25:17 +0100

    Initial commit

---
+++ tests/Packman/LoaderTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Packman
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://github.com/padraic/packman/master/LICENSE
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to padraic@php.net so we can send you a copy immediately.
+ *
+ * @category   Packman
+ * @package    Packman
+ * @subpackage UnitTests
+ * @copyright  Copyright (c) 2011 Pádraic Brady (http://blog.astrumfutura.com)
+ * @license    http://github.com/padraic/packman/blob/master/LICENSE New BSD License
+ */
+
+class Packman_LoaderTest extends PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+        spl_autoload_unregister('\Packman\Loader::loadClass');
+    }
+
+    public function testCallingRegisterRegistersSelfAsSplAutoloaderFunction()
+    {
+        require_once 'Packman/Loader.php';
+        $loader = new \Packman\Loader;
+        $loader->register();
+        $expected = array($loader, 'loadClass');
+        $this->assertTrue(in_array($expected, spl_autoload_functions()));
+    }
+
+    public function tearDown()
+    {
+        spl_autoload_unregister('\Packman\Loader::loadClass');
+        $loader = new \Packman\Loader;
+        $loader->register();
+    }
+
+}
